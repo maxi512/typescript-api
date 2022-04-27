6e5d9cd6b5df23ca342cf964a1e2d785abb5ea90
Horus98 <42451015+Horus98@users.noreply.github.com>
2022-04-27 17:31:18 -0300

    Corse setup

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,14 @@ import { container } from 'tsyringe';
 import express from 'express';
 import UserRoutes from './routes/users';
 import { connect } from 'mongoose';
+import cors from 'cors';
+
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
 const port = process.env.PORT || 3010;
 
 app.get('/', (_req, res) => {
