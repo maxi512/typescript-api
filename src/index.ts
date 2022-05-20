805d4dd50d8ba5d5cdac82cdd7d0728c537f6e0e
Maxi Montenegro <maximontenegro512@gmail.com>
2022-05-20 15:16:50 -0300

    add dotenv

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,12 @@ import express from 'express';
 import db from './db';
 import UserRoutes from './routes/users';
 import cors from 'cors';
+import dotenv from 'dotenv';
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+dotenv.config();
 
 const port = process.env.PORT || 4000;
 
