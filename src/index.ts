959039d60239bb9d9bd86132516ad2a37f72fcd1
Maxi Montenegro <maximontenegro512@gmail.com>
2022-06-05 19:07:23 -0300

    change port

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-const port = 4000;
+const port = process.env.PORT || 4000;
 
 app.get('/', (_req, res) => {
 	res.send('Hello World!!!');
