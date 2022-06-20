cfa29e0979059c0e055bb6f662be22caddc194cc
Maxi Montenegro <maximontenegro512@gmail.com>
2022-06-20 18:06:24 -0300

    :fix:Add change

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,7 @@ app.use(cors());
 const port = process.env.PORT || 4000;
 
 app.get('/', (_req, res) => {
-	res.send('Hello World!!!!!!!!asdasdasddasasdas!!');
+	res.send('Hello World!!!!!!!!asdasdasddasa111sdas!!');
 });
 
 db.connectDB();
